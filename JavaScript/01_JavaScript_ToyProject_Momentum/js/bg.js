6382c224090db4f363efd356340f9de0df11b9f8
kwak-bs <iam-bs@naver.com>
2021-01-01 14:33:12 +0900

    FIX : unsplash api 수정

--- JavaScript/01_JavaScript_ToyProject_Momentum/js/bg.js
+++ JavaScript/01_JavaScript_ToyProject_Momentum/js/bg.js
@@ -4,7 +4,7 @@ const locationContainer = document.querySelector(".js-location span");
 function paintImage() {
   const image = new Image();
   // 가입없이 밑에 이것만 주면 무료로 랜덤 사진 API 사용가능
-  image.src = "https://source.unsplash.com/featured/?Film,Travel,Food,Drink";
+  image.src = "https://source.unsplash.com/featured/?landscape,travel";
   image.classList.add("bgImage"); //css에서 가져옴
   body.appendChild(image);
 }
