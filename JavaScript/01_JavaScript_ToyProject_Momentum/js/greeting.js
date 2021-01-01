ad07f07b5b979e919b4f174e8fbce50e83380c83
kwak-bs <iam-bs@naver.com>
2021-01-01 12:01:37 +0900

    REFACTORING : 2021.01.01

--- JavaScript/01_JavaScript_ToyProject_Momentum/js/greeting.js
+++ JavaScript/01_JavaScript_ToyProject_Momentum/js/greeting.js
@@ -1,43 +1,59 @@
-const form = document.querySelector(".js-form");
-const input = form.querySelector("input");
-const greeting = document.querySelector(".js-greetings");
+const nameContainer = document.querySelector(".js-name");
 
-const USER_LS = "currentUser"; //LocalStorage
-const SHOWING_CN = "showing"; // ClassName
-
-function saveName(text) {
-  localStorage.setItem(USER_LS, text); // key - value로 로컬 스토리지에 저장
+function paintName(name) {
+  nameContainer.innerHTML = "";
+  const title = document.createElement("span");
+  title.className = "name__text";
+  title.innerHTML = `Hello ${name}`;
+  nameContainer.appendChild(title);
 }
 
 function handleSubmit(event) {
   event.preventDefault(); // form 입력 시 새로고침 방지. 새로고침이 default 이벤트임
-  const currentValue = input.value; // 입력값 저장
-  paintGreeting(currentValue);
-  saveName(currentValue);
+  const form = event.target;
+  const input = form.querySelector("input");
+  const value = input.value; // 입력값 저장
+
+  localStorage.setItem("username", value); // key - value 형태로 로컬 스토리지에 저장.
+  paintName(value);
 }
 
-function askForName() {
-  form.classList.add(SHOWING_CN); // css의 showing 클래스 불러옴
+function paintInput() {
+  // input , form 둘이 세트임
+
+  const input = document.createElement("input");
+  input.placeholder = "이름을 입력하세요.";
+  input.type = "text";
+  input.className = "name__input";
+
+  const form = document.createElement("form");
   form.addEventListener("submit", handleSubmit);
+  form.appendChild(input);
+  nameContainer.appendChild(form);
 }
 
-function paintGreeting(text) {
-  // 유저가 있으면 form 삭제하고 <h4>태그 보여줌
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
-}
+// function askForName() {
+//   form.classList.add(SHOWING_CN); // css의 showing 클래스 불러옴
+//   form.addEventListener("submit", handleSubmit);
+// }
+
+// function paintGreeting(text) {
+//   // 유저가 있으면 form 삭제하고 <h4>태그 보여줌
+//   form.classList.remove(SHOWING_CN);
+//   greeting.classList.add(SHOWING_CN);
+//   greeting.innerText = `Hello ${text}`;
+// }
 
 function loadName() {
-  // 로컬스토리지의 USER_LS 값 불러와서 저장
-  const currentUser = localStorage.getItem(USER_LS);
+  // 로컬스토리지의 username 값 불러와서 저장
+  const name = localStorage.getItem("username");
 
-  if (currentUser === null) {
+  if (name === null) {
     // 유저가 없는 경우
-    askForName();
+    paintInput();
   } else {
     // 유저가 있는 경우
-    paintGreeting(currentUser);
+    paintName(name);
   }
 }
 
--- JavaScript/01_JavaScript_ToyProject_Momentum/js/todo.js
+++ JavaScript/01_JavaScript_ToyProject_Momentum/js/todo.js
@@ -1,100 +1,104 @@
-const toDoForm = document.querySelector(".js-todoForm");
-const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector(".js-todoList");
-
-const TODOS_LS = "toDos";
+const form = document.querySelector(".js-to-do");
+const input = document.querySelector(".js-add-to-do");
+const list = document.querySelector(".js-list");
 
 let toDos = [];
 
-function filterFn(toDo) {
-  //filter는 array의 모든 아이템을 통해 함수를 실행하고
-  //true인 아이템들만 가지고 새로운 array를 만든다.
-  return toDo.id === 1;
-}
-
-function deleteTodo(event) {
+function handleDelete(event) {
   //console.dir(event.target)에서 parentNode찾으면 부모 찾을수 있음.
   //console.dir(event.target);
   //console.log(event.target.parentNode);
 
-  const btn = event.target;
-  const li = btn.parentNode;
-  toDoList.removeChild(li); // 삭제
+  const target = event.target;
+  const li = target.parentElement;
+  const ul = li.parentElement;
+  const toDoId = li.id;
 
-  const cleanToDos = toDos.filter(function (toDo) {
-    return toDo.id !== parseInt(li.id);
+  ul.removeChild(li); // 삭제
+
+  toDos = toDos.filter(function (toDo) {
+    //filter는 array의 모든 아이템을 통해 함수를 실행하고
+    //true인 아이템들만 가지고 새로운 array를 만든다.
+    console.log(toDo.id);
+    console.log(toDoId);
+    return toDo.id !== parseInt(toDoId) + 1; //toDoId는 0부터 기준이다. 근데 toDo.id는 1부터 id를 잡기 때문에 +1 해줘야한다.
   });
-  toDos = cleanToDos;
-  saveToDos();
+
+  persistToDos();
+}
+
+let idNum = 0;
+
+//투두 추가
+function addToDo(text) {
+  const toDo = document.createElement("li"); // li 생성
+  toDo.className = "toDo";
+  toDo.id = idNum;
+  idNum++;
+
+  const deleteBtn = document.createElement("span"); // span 태그 생성
+  deleteBtn.innerHTML = "❌"; // 버튼에 이모지 넣어주기,  <meta charset="utf-8"/> 이거 해줘야 이모지 보임.
+  deleteBtn.className = "toDo__button";
+  deleteBtn.addEventListener("click", handleDelete); // 버튼에 삭제 이벤트 추가.
+
+  const label = document.createElement("label");
+  label.innerHTML = text; // 인자로 받은 text 넣어주기
+
+  toDo.appendChild(deleteBtn); // 삭제 버튼 추가
+  toDo.appendChild(label);
+  list.appendChild(toDo);
+  saveToDo(text);
+}
+
+function saveToDo(text) {
+  const toDoObject = {
+    id: idNum, // length로 id를 줘버리면 todo를 삭제하고 추가시 id가 중복되는 현상발생함. 그래서 id값만 지정할 변수 생성.
+    value: text,
+  };
+  toDos.push(toDoObject);
+  persistToDos();
 }
 
 // 로컬 스토리지에 투두저장
-function saveToDos() {
-  //자바스크립트는 local storage에 있는 모든 데이터를 string으로 저장하려고 한다.
+function persistToDos() {
+  // 자바스크립트는 local storage에 있는 모든 데이터를 string으로 저장하려고 한다.
   // 그래서 우리 자바스크립트 object를 string으로 바꿔줘야한다.
   // JSON.stringfy();
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-}
-
-let idNum = 1;
-
-// 투두 보여주기
-function paintToDo(text) {
-  const li = document.createElement("li"); // li 생성
-  const delBtn = document.createElement("button"); // 버튼 생성
-  const span = document.createElement("span"); // span 태그 생성
-  const newId = idNum;
-  idNum++;
-
-  delBtn.innerText = "❌"; // 버튼에 이모지 넣어주기,  <meta charset="utf-8"/> 이거 해줘야 이모지 보임.
-  delBtn.addEventListener("click", deleteTodo); // 버튼에 삭제 이벤트 추가.
-
-  span.innerText = text; // span 태그에 인자로 받은 text 넣어주기
-
-  li.appendChild(delBtn); // li에 삭제 버튼 추가
-  li.appendChild(span); // li에 span 태그 추가
-  li.id = newId; // li에도 id 부여하기, 나중에 지울때 편리하기 위해
-
-  toDoList.appendChild(li); // ul 태그에 하위(li) 추가.
-
-  const toDoObj = {
-    text: text,
-    id: newId, // length로 id를 줘버리면 todo를 삭제하고 추가시 id가 중복되는 현상발생함. 그래서 id값만 지정할 변수 생성.
-  };
-
-  toDos.push(toDoObj);
-  saveToDos();
+  const stringToDo = JSON.stringify(toDos);
+  localStorage.setItem("toDos", stringToDo);
 }
 
 // 제출함수
-function handleSubmit(event) {
+function onSubmit(event) {
   event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
-  toDoInput.value = ""; // 입력 후 다시 빈값
+  const value = input.value;
+  input.value = ""; // 입력 후 다시 빈값
+  addToDo(value);
 }
 
 //투두 불러오기
 function loadToDos() {
-  const loadedtoDos = localStorage.getItem(TODOS_LS);
-  if (loadedtoDos !== null) {
+  const loadedToDos = localStorage.getItem("toDos");
+  if (loadedToDos !== null) {
     //불러온게 string이라 또 객체로 변환해줘야함.
     //JSON.parse();
-    const parsedTodos = JSON.parse(loadedtoDos);
+    const parsedToDos = JSON.parse(loadedToDos);
 
     //forEach는 array를 위한 함수
     // parsedTodos 각각에 대해서 실행해줄 함수
-    parsedTodos.forEach(function (toDo) {
+    parsedToDos.forEach(function (toDo) {
       // 로컬스토리지에 저장되어 있는 todo를 불러와서 보여줌
-      paintToDo(toDo.text);
+      addToDo(toDo.value);
     });
   }
+  return;
 }
 
 //시작
 function init() {
   loadToDos();
-  toDoForm.addEventListener("submit", handleSubmit);
 }
+
+form.addEventListener("submit", onSubmit);
 
 init();
