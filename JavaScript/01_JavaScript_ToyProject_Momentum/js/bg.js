75f0c1213dbaaa1eedce47cf8ca12ba9b3367902
kwak-bs <iam-bs@naver.com>
2021-01-01 14:13:28 +0900

    REFACTORING : 2021.01.01

--- JavaScript/01_JavaScript_ToyProject_Momentum/js/bg.js
+++ JavaScript/01_JavaScript_ToyProject_Momentum/js/bg.js
@@ -1,23 +1,16 @@
 const body = document.querySelector("body");
+const locationContainer = document.querySelector(".js-location span");
 
-const IMG_NUMBER = 5;
-
-function paintImage(imgNumber) {
+function paintImage() {
   const image = new Image();
-  image.src = `images/${imgNumber}.jpg`;
+  // 가입없이 밑에 이것만 주면 무료로 랜덤 사진 API 사용가능
+  image.src = "https://source.unsplash.com/featured/?Film,Travel,Food,Drink";
   image.classList.add("bgImage"); //css에서 가져옴
   body.appendChild(image);
 }
 
-function getRandom() {
-  // floor 는 버림 ceil은 올림
-  const number = Math.floor(Math.random() * IMG_NUMBER + 1);
-  return number;
-}
-
 function init() {
-  const randomNumber = getRandom();
-  paintImage(randomNumber);
+  paintImage();
 }
 
 init();
--- JavaScript/01_JavaScript_ToyProject_Momentum/js/weather.js
+++ JavaScript/01_JavaScript_ToyProject_Momentum/js/weather.js
@@ -2,28 +2,20 @@ const weather = document.querySelector(".js-weather");
 
 //OpenWeather API 키 가져옴
 const API_KEY = "b94dda977bd60bb0acb1bba7d9f2a81a";
-const COORDS = "coords";
+const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?";
 
-function getWeather(lat, lon) {
+function getWeather(coordsObj) {
   // 데이터 가져오기
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    `${WEATHER_API}lat=${coordsObj.latitude}&lon=${coordsObj.longitude}&appid=${API_KEY}&units=metric`
   )
-    .then(function (response) {
-      // 데이터가 우리한테 넘어 왔을때(데이터가 완전히 들어온 다음 호출)
-      return response.json();
-    })
+    .then((response) => response.json()) // 데이터가 우리한테 넘어 왔을때(데이터가 완전히 들어온 다음 호출), 화살표 함수
     .then(function (json) {
       // response.json()도 pending(대기) 완료 되면 json 불러옴.
       const temperature = json.main.temp; // 온도 저장
       const place = json.name; // 위치 저장
-      weather.innerText = `${temperature} @ ${place}`; // span에 넣어주기
+      weather.innerText = `${Math.floor(temperature)}° @ ${place}`; // span에 넣어주기
     });
-}
-
-// 로컬스토리지에 저장
-function saveCoords(coordsObj) {
-  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 // 허용시 위도, 경도 저장
@@ -35,33 +27,35 @@ function handleGeoSuccess(position) {
     latitude,
     longitude,
   };
-  saveCoords(coordsObj);
-  getWeather(latitude, longitude);
+  // 로컬스토리지에 저장
+  localStorage.setItem("coords", JSON.stringify(coords));
+  getWeather(coordsObj);
 }
 
-// 허용x시 에러 처리
-function handleGeoError() {
+// 허용x시 처리
+function handleGeoFailure() {
   console.log("Can't access geo location");
 }
 
-// 허용하냐고 물어봄
-function askForCoords() {
-  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
-}
-
 // 날씨 정보 불러오기. 없으면 권한 물어봄.
-function loadCoords() {
-  const loadedCoords = localStorage.getItem(COORDS);
-  if (loadedCoords === null) {
-    askForCoords();
+function loadWeather() {
+  const currentCoords = localStorage.getItem("coords");
+  if (currentCoords !== null) {
+    // 현재 날씨 정보가 있으면 객체로 변환후 넘겨줌
+    const parsedCoords = JSON.parse(currentCoords);
+    getWeather(parsedCoords);
+    return;
   } else {
-    const parsedCoords = JSON.parse(loadedCoords);
-    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    navigator.geolocation.getCurrentPosition(
+      // 권한 허용, 거부
+      handleGeoSuccess,
+      handleGeoFailure
+    );
   }
 }
 
 function init() {
-  loadCoords();
+  loadWeather();
 }
 
 init();
