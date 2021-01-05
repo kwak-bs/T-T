b7be5e6f1d477d303562ada3b6d4d2e08f1a20bb
kwak-bs <iam-bs@naver.com>
2021-01-05 09:46:13 +0900

    FEAT : 로컬스토리지에 이름 삭제 기능 추가

--- JavaScript/ToyProject_01_Momentum/js/greeting.js
+++ JavaScript/ToyProject_01_Momentum/js/greeting.js
@@ -1,13 +1,34 @@
 const nameContainer = document.querySelector(".js-name");
 
+//로컬 스토리지에 이름이 있는 경우
 function paintName(name) {
   nameContainer.innerHTML = "";
   const title = document.createElement("span");
   title.className = "name__text";
   title.innerHTML = `안녕하세요, ${name}님.`;
+
+  const deleteName = document.createElement("span");
+  deleteName.innerHTML = "🚫";
+  deleteName.className = "Name__button";
+  deleteName.addEventListener("click", handleNameDelete);
+
   nameContainer.appendChild(title);
+  nameContainer.appendChild(deleteName);
 }
 
+// 하 처음에 handleDelete()로 이름 지었다가, 만들자마자 삭제가 안되는 이슈가 발생해서 삽질좀함.
+// 아마 todo.js에 있는 handleDelete()랑 겹쳐서 그런듯.
+// 함수 네이밍할 때 중복되지 않도록 주의하자.
+function handleNameDelete() {
+  const name = localStorage.getItem("username");
+  if (name !== null) {
+    localStorage.removeItem("username"); // 로컬 스토리지에서 삭제
+  }
+  nameContainer.innerHTML = ""; // 초기화
+  paintInput(); // 이름 입력 창 띄우기
+}
+
+//이름입력 후 제출처리
 function handleSubmit(event) {
   event.preventDefault(); // form 입력 시 새로고침 방지. 새로고침이 default 이벤트임
   const form = event.target;
@@ -18,9 +39,9 @@ function handleSubmit(event) {
   paintName(value);
 }
 
+// 로컬 스토리지에 이름이 없는 경우
 function paintInput() {
   // input , form 둘이 세트임
-
   const input = document.createElement("input");
   input.placeholder = "이름을 입력하세요.";
   input.type = "text";
@@ -31,18 +52,6 @@ function paintInput() {
   form.appendChild(input);
   nameContainer.appendChild(form);
 }
-
-// function askForName() {
-//   form.classList.add(SHOWING_CN); // css의 showing 클래스 불러옴
-//   form.addEventListener("submit", handleSubmit);
-// }
-
-// function paintGreeting(text) {
-//   // 유저가 있으면 form 삭제하고 <h4>태그 보여줌
-//   form.classList.remove(SHOWING_CN);
-//   greeting.classList.add(SHOWING_CN);
-//   greeting.innerText = `Hello ${text}`;
-// }
 
 function loadName() {
   // 로컬스토리지의 username 값 불러와서 저장
